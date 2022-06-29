951d41a5f0eeb1215d2366b855ce5b3b27c2da66
eddtn <eddietonsagie@gmail.com>
2022-06-29 20:20:03 +0100

    first commit

---
+++ lib/views/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffE8EAF6),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Color(0xffD897FC),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: SvgPicture.network('https://mppxs.csb.app/happy.svg'),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Discover Your \n Dream job Here',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Explore all the most exiting jobs roles \n based on your  interest and stufy major',
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              Container(
+                height: 70,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(children: [
+                  Container(
+                    height: 70,
+                    width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
